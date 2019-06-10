2d4f74ef4e34b10c9e07c1b7a49c5f3a51769793
Daniel Schneider <daschne8@gmail.com>
2019-06-10 18:59:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function scuberGreetingForFeet(distance){
     case distance <= 400:
       response = 'This one is on me!';
       break;
-    case distance >= 2000:
+    case  2000 <= distance <= 2500 :
       response = 'I will gladly take your thirty bucks.'
       break;
     case distance > 2500:
