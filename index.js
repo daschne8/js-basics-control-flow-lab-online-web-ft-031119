1a81de783c2be7078b38df340b66f4ed036ea2db
Daniel Schneider <daschne8@gmail.com>
2019-06-10 17:40:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let response
+  switch(distance){
+    case distance <= 400:
+      response = 'This one is on me!';
+      break;
+    case distance >= 2000:
+      
+      break;
+  }
 }
 
 function ternaryCheckCity(){
